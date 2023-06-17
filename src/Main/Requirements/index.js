de4611beb0a9e831b12fd195e56d23f8aef1e10f
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2023-06-17 14:32:19 -0300

    ongoing project

--- src/Main/Requirements/index.js
+++ src/Main/Requirements/index.js
@@ -1,32 +1,101 @@
-import { Flex, Box, Accordion, AccordionItem, AccordionButton, AccordionIcon, AccordionPanel, useBreakpointValue } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import {
+  Flex,
+  Box,
+  Link,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+  useBreakpointValue,
+  useMediaQuery,
+} from '@chakra-ui/react';
+
+import whatsappImage from '../../images/whatsapp.svg';
 
 export default function Requirements() {
   const isMobile = useBreakpointValue({ base: true, md: false, sm: true });
   const isDesktop = useBreakpointValue({ base: false, md: true, lg: true });
+  const isDisplay = useMediaQuery("(max-width: 1440px)");
+
+  const [showWhatsApp, setShowWhatsApp] = useState(false);
+  const [lastScrollPosition, setLastScrollPosition] = useState(0);
 
   const getFlexWrap = () => {
-    return isMobile && !isDesktop ? "wrap" : "nowrap";
+    return isMobile && !isDesktop ? 'wrap' : 'nowrap';
   };
 
+  const handleAlert = () => {
+    alert('Você clicou no botão do WhatsApp');
+  };
+
+  const handleScroll = () => {
+    const currentScrollPosition = window.pageYOffset;
+
+    if (
+      currentScrollPosition > lastScrollPosition &&
+      currentScrollPosition > window.innerHeight &&
+      currentScrollPosition < document.getElementById('requirements').offsetTop
+    ) {
+      setShowWhatsApp(true);
+    } else if (currentScrollPosition < lastScrollPosition) {
+      setShowWhatsApp(true);
+    } else {
+      setShowWhatsApp(false);
+    }
+
+    setLastScrollPosition(currentScrollPosition);
+  };
+
+  const handleResize = () => {
+    const maxWidth = 1440;
+    const windowWidth = window.innerWidth;
+
+    setShowWhatsApp(windowWidth <= maxWidth);
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  // const getFlexWrap = () => {
+  //   return isMobile && !isDesktop ? 'wrap' : 'nowrap';
+  // };
+
+  window.addEventListener('scroll', function () {
+    const element = document.getElementById('requirements');
+    const rect = element.getBoundingClientRect();
+    const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+
+    if (!isVisible) {
+      setShowWhatsApp(false);
+    }
+  });
+
   return (
-    <Flex p={isMobile ? "1rem" : "0.2rem"} mb={"3rem"}  mt={isMobile ? "7rem" : "7rem"} justify="center">
+    <Flex p={isMobile ? '1rem' : '0.2rem'} m={isMobile ? '5rem 0' : '9rem 0'} justify="center" id="requirements">
       <Box w="100%" position="relative">
-        <Flex
-          p={{ base: "0", md: "0 1rem" }}
-          align="center"
-          justify="center"
-          mt={isMobile ? "9rem" : "0.3rem"}
-        >
+        <Flex p={{ base: '0', md: '0 1rem' }} align="center" justify="center" mt={isMobile ? '9rem' : '0.3rem'}>
           <Flex
             border="3px solid #FED600"
             borderRadius="15px"
-            w={isMobile ? "100%" : "70%"}
-            mt={{ base: "-12rem", md: "0" }}
+            w={isMobile ? '100%' : '70%'}
+            mt={{ base: '-12rem', md: '0' }}
             direction="column"
             gap="1rem"
-            p={{ base: "0", md: "0 1rem" }}
+            p={{ base: '0', md: '0 1rem' }}
           >
             <Flex
+              className="AQUI"
               direction="column"
               w="100%"
               align="center"
@@ -41,7 +110,7 @@ export default function Requirements() {
               <Accordion allowToggle w="100%">
                 <AccordionItem>
                   <h2>
-                    <AccordionButton color="#000" _hover={{ backgroundColor: "none" }}>
+                    <AccordionButton color="#000" _hover={{ backgroundColor: 'none' }}>
                       <Box as="span" flex="1" textAlign="left" color="#000">
                         Quais taxas são cobradas?
                       </Box>
@@ -49,13 +118,17 @@ export default function Requirements() {
                     </AccordionButton>
                   </h2>
                   <AccordionPanel pb={4} color="#000">
-                    Em nossa empresa, aplicamos uma taxa fixa de 6% em todos os produtos disponíveis. Essa taxa é incorporada ao preço final para garantir uma operação eficiente e sustentável. Estamos comprometidos em fornecer um serviço de qualidade a todos os nossos clientes e garantir transparência em relação às taxas aplicadas. Se você tiver alguma dúvida adicional sobre as taxas ou qualquer outro aspecto dos nossos serviços, estamos à disposição para ajudar.
+                    Em nossa empresa, aplicamos uma taxa fixa de 6% em todos os produtos disponíveis. Essa taxa é
+                    incorporada ao preço final para garantir uma operação eficiente e sustentável. Estamos comprometidos em
+                    fornecer um serviço de qualidade a todos os nossos clientes e garantir transparência em relação às taxas
+                    aplicadas. Se você tiver alguma dúvida adicional sobre as taxas ou qualquer outro aspecto dos nossos
+                    serviços, estamos à disposição para ajudar.
                   </AccordionPanel>
                 </AccordionItem>
 
                 <AccordionItem>
                   <h2>
-                    <AccordionButton color="#000" _hover={{ backgroundColor: "none" }}>
+                    <AccordionButton color="#000" _hover={{ backgroundColor: 'none' }}>
                       <Box as="span" flex="1" textAlign="left" color="#000">
                         O Mumu Delivery pode cancelar minha conta a qualquer momento?
                       </Box>
@@ -63,13 +136,16 @@ export default function Requirements() {
                     </AccordionButton>
                   </h2>
                   <AccordionPanel pb={4} color="#000">
-                    Sim, o Mumu Delivery reserva-se o direito de cancelar uma conta caso haja violação grave das políticas ou se as ações do usuário forem prejudiciais à plataforma ou a outros usuários. No entanto, priorizamos a resolução de problemas e a comunicação aberta, buscando encontrar soluções em conjunto. Nosso objetivo é oferecer um serviço de qualidade e um ambiente seguro para todos os usuários.
+                    Sim, o Mumu Delivery reserva-se o direito de cancelar uma conta caso haja violação grave das políticas
+                    ou se as ações do usuário forem prejudiciais à plataforma ou a outros usuários. No entanto, priorizamos
+                    a resolução de problemas e a comunicação aberta, buscando encontrar soluções em conjunto. Nosso objetivo
+                    é oferecer um serviço de qualidade e um ambiente seguro para todos os usuários.
                   </AccordionPanel>
                 </AccordionItem>
 
                 <AccordionItem>
                   <h2>
-                    <AccordionButton color="#000" _hover={{ backgroundColor: "none" }}>
+                    <AccordionButton color="#000" _hover={{ backgroundColor: 'none' }}>
                       <Box as="span" flex="1" textAlign="left" color="#000">
                         Quais documentos preciso para me cadastrar?
                       </Box>
@@ -77,13 +153,14 @@ export default function Requirements() {
                     </AccordionButton>
                   </h2>
                   <AccordionPanel pb={4} color="#000">
-                    Para se cadastrar no Mumu Delivery, você precisará informar seu CPF, nome completo, logradouro e CEP. Essas informações são necessárias para criar sua conta e garantir uma entrega eficiente dos pedidos.
+                    Para se cadastrar no Mumu Delivery, você precisará informar seu CPF, nome completo, logradouro e CEP.
+                    Essas informações são necessárias para criar sua conta e garantir uma entrega eficiente dos pedidos.
                   </AccordionPanel>
                 </AccordionItem>
 
                 <AccordionItem>
                   <h2>
-                    <AccordionButton color="#000" _hover={{ backgroundColor: "none" }}>
+                    <AccordionButton color="#000" _hover={{ backgroundColor: 'none' }}>
                       <Box as="span" flex="1" textAlign="left" color="#000">
                         Em quais cidades tem Mumu Delivery?
                       </Box>
@@ -91,13 +168,16 @@ export default function Requirements() {
                     </AccordionButton>
                   </h2>
                   <AccordionPanel pb={4} color="#000">
-                    O Mumu Delivery está disponível em diversas cidades, incluindo grandes capitais como São Paulo e Rio de Janeiro. Além dessas, o serviço também está presente em outras capitais e regiões metropolitanas do Brasil. A expansão do Mumu Delivery tem buscado atender cada vez mais clientes em diferentes localidades, proporcionando conveniência e facilidade na entrega de produtos.
+                    O Mumu Delivery está disponível em diversas cidades, incluindo grandes capitais como São Paulo e Rio de
+                    Janeiro. Além dessas, o serviço também está presente em outras capitais e regiões metropolitanas do
+                    Brasil. A expansão do Mumu Delivery tem buscado atender cada vez mais clientes em diferentes localidades,
+                    proporcionando conveniência e facilidade na entrega de produtos.
                   </AccordionPanel>
                 </AccordionItem>
 
                 <AccordionItem>
                   <h2>
-                    <AccordionButton color="#000" _hover={{ backgroundColor: "none" }}>
+                    <AccordionButton color="#000" _hover={{ backgroundColor: 'none' }}>
                       <Box as="span" flex="1" textAlign="left" color="#000">
                         Quanto tempo leva para meu cadastro ter aceito no aplicativo?
                       </Box>
@@ -105,11 +185,65 @@ export default function Requirements() {
                     </AccordionButton>
                   </h2>
                   <AccordionPanel pb={4} color="#000">
-                    O processo de aprovação do cadastro no aplicativo do Mumu Delivery é rápido e geralmente leva apenas alguns minutos. Após preencher corretamente todas as informações necessárias, como CPF, nome completo, endereço e CEP, o sistema realiza uma verificação e, se todos os dados estiverem corretos, seu cadastro será aprovado quase instantaneamente. Assim, você poderá começar a usar o Mumu Delivery e desfrutar dos seus serviços de entrega de forma ágil e eficiente.
+                    O processo de aprovação do cadastro no aplicativo do Mumu Delivery é rápido e geralmente leva apenas
+                    alguns minutos. Após preencher corretamente todas as informações necessárias, como CPF, nome completo,
+                    endereço e CEP, o sistema realiza uma verificação e, se todos os dados estiverem corretos, seu cadastro
+                    será aprovado quase instantaneamente. Assim, você poderá começar a usar o Mumu Delivery e desfrutar dos
+                    seus serviços de entrega de forma ágil e eficiente.
                   </AccordionPanel>
                 </AccordionItem>
               </Accordion>
             </Flex>
+            {showWhatsApp && (
+              <div
+                style={{
+                  position: 'fixed',
+                  bottom: isMobile ? '47rem' : isDesktop ? '30rem' : '4rem',
+                  right: isMobile ? '1rem' : isDesktop ? '5rem' : '5rem',
+                  zIndex: '9999',
+                }}
+              >
+                {isDisplay && (
+                  <Link
+                    // href="https://seu-link-do-whatsapp.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    alt="Botão para abrir conversa no WhatsApp"
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    borderRadius="50%"
+                    width="60px"
+                    height="60px"
+                    maxWidth="1440px"
+                  >
+                    <img
+                      src={showWhatsApp ? whatsappImage : null}
+                      alt="WhatsApp"
+                      onClick={handleAlert}
+                      style={{ display: showWhatsApp ? 'block' : 'none' }}
+                    />
+                  </Link>
+                )}
+                {!isDisplay && (
+                  <Link
+                    // href="https://seu-link-do-whatsapp.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    alt="Botão para abrir conversa no WhatsApp"
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    borderRadius="50%"
+                    width="60px"
+                    height="60px"
+                    maxWidth="1440px"
+                  >
+                    <img src={whatsappImage} alt="WhatsApp" onClick={handleAlert} />
+                  </Link>
+                )}
+              </div>
+            )}
           </Flex>
         </Flex>
       </Box>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,51 +1,114 @@
-import { Flex, Center, Link, Box, Image, useMediaQuery } from "@chakra-ui/react";
+import { Flex, Text, Box, Image, Link, useMediaQuery } from "@chakra-ui/react";
+import mumulogo from '../../images/mumu3 1.svg'
+import facebookImg from '../../images/facebook_svgrepo.com.svg'
+import instagramImg from '../../images/instagram-1_svgrepo.com.svg'
 import appStore from '../../images/AppStore.svg'
 import GooglePlayStore from '../../images/GooglePlayStore.svg'
 
-import mumulogo from '../../images/mumu3 1.svg'
-
 export default function Footer() {
+
   const [isMobile] = useMediaQuery("(max-width: 768px)");
 
   return (
-    <Flex flex={1} direction={"row"} justify={"space-between"} bg={"#E5E5E587"}>
-      <Center
-        w={"100%"}
-        justifyContent={"space-between"}
-        p={4}
-        alignItems={"center"}
-        m={isMobile ? "0" : "0 1rem"}
+    <Box bg="#E5E5E587">
+      <Flex
+        direction={isMobile ? "column" : "row"}
+        justify="center"
+        align="center"
+        p="1rem"
+        mx="auto"
+        mt="2rem"
+        flexWrap={isMobile ? "none" : "wrap"}
+        gap={isMobile ? "0" : "2rem"}
+        alignItems={isMobile ? "center" : "baseline"}
       >
-        <Flex alignItems={"center"}>
-          <Link top={"/"} mr={5}>
-            <Image src={mumulogo} alt="logo" />
+        <Flex
+          direction="column"
+          align="center"
+          mb={{ base: "2rem", md: "0" }}
+          w={isMobile ? "100%" : "21%"}
+        >
+          <Link>
+            <Image src={mumulogo} alt="logo" /> 
           </Link>
-          <Box display={isMobile ? "flex" : "flex"} direction={isMobile ? "column" : "row"} gap={isMobile ? "5rem" : "1em"}>
-            
-              <Box fontSize={isMobile ? "10px" : ""} _hover={{ color: "#000" }} color={"#f00"} >Sobre o Mumu Delivery</Box>
-
-            
-              <Box fontSize={isMobile ? "10px" : ""} _hover={{ color: "#000000" }} color={"#f00"}>Termos e políticas</Box>
-            
-            
-              <Flex direction={"column"}>
-                <Box fontSize={isMobile ? "10px" : ""} _hover={{ color: "#000" }} color={"#f00"}>Já baixou nossos aplicativos?
-                </Box>
-
-                <Link>
-                    <Image src={appStore} alt="Link aplicativo appStore" />
-                </Link>
-
-                <Link>
-                    <Image src={GooglePlayStore} alt="Link aplicativo GooglePlayStore" />
-                </Link>
-
-              </Flex>  
-    
+          <Box fontSize={5}  alignSelf={isMobile ? "none" : "flex-end"}>
+            <Text fontSize={13} color="#000">
+              contato@mumudelivery.com
+            </Text>
+            <Text fontSize={13} color="#000">
+              +55 (21) 99999-9999
+            </Text>
           </Box>
+          <Flex mt="1rem">
+            <Link mr="0.5rem">
+              <Image src={facebookImg} alt="imagem Facebook" />
+            </Link>
+            <Link>
+              <Image src={instagramImg} alt="imagem Instagram" />
+            </Link>
+          </Flex>
         </Flex>
-      
-      </Center>
-    </Flex>
+
+        <Flex
+          direction="column"
+          align={isMobile ? "center" : "flex-start"}
+          mb={{ base: "2rem", md: "0" }}
+        >
+          <Text fontWeight={700} mb="0.5rem" color={"#f00"}>
+            Sobre o Mumu Delivery
+          </Text>
+          <Link color="#000">Me ajuda, Mumu</Link>
+          <Link color="#000">Cidades que atendemos</Link>
+          <Link color="#000">Trabalhe conosco</Link>
+          <Link color="#000">Quero ser parceiro</Link>
+          <Link color="#000">Comprar cartão presente</Link>
+        </Flex>
+
+        <Flex
+          direction="column"
+          align={isMobile ? "center" : "flex-start"}
+          mb={{ base: "0rem", md: "0" }}
+        >
+          <Text fontWeight={700} mb="0.5rem" color={"#f00"} >
+            Termos e Política
+          </Text>
+          <Link color="#000">Termos de Uso</Link>
+          <Link color="#000">Política de Privacidade</Link>
+        </Flex>
+
+
+        <Flex
+          direction="column"
+          align={isMobile ? "center" : "flex-start"}
+          mb={{ base: "2rem", md: "0" }}
+        >
+          <Text fontWeight={700} mb="0.5rem" color={"#f00"} >
+            Termos e Política
+          </Text>
+          <Link>
+            <Image src={appStore} alt="Download on the App Store" />
+          </Link>
+          <Link>
+            <Image src={GooglePlayStore} alt="Get it on Google Play" />
+          </Link>
+        </Flex>
+     
+      </Flex>
+        <Flex
+          direction={{ base: "column", md: "row" }}
+          align={isMobile ?  "center" :  "flex-end"}
+          justify="center"
+          p="2rem"
+          // bg="#f9f9f9"
+          borderTop="1px solid rgba(0,0,0,0.1)"
+          borderWidth={"1%"}
+        >
+          <Text fontSize={12} color="#000" textAlign="center" mb={{ base: "1rem", md: "0" }}>
+            © 2023 Mumu Delivery. Todos os direitos reservados.
+          </Text>
+
+        </Flex>
+
+    </Box>
   );
 }
