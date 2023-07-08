7bef7e8e958796dd7029fce95a2f79dee2b2c2e8
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2023-07-08 11:37:49 -0300

    update project

--- src/Main/Proposal/index.js
+++ src/Main/Proposal/index.js
@@ -24,140 +24,143 @@ export default function Proposal() {
   };
 
   return (
-    <Flex p="0.2rem" mt={isMobile ? "2rem" : "5rem"} id='proposal'>
-      <Box w="100%" position="relative">
-        <Flex
-          direction="column"
-          p="0 1rem"
-          w={isDesktop ? "100%" : "100%"}
-          align="center"
-          justify="center"
-          mt={isMobile ? "4rem" : "0.3rem"}
-          flexWrap={getFlexWrap()}
-          position="relative"
-          gap="4rem"
-          textAlign="center"
-          justifyContent="center"
-        >
-          <Box
-            flex="1"
-            textAlign="start"
-            alignItems="flex-start"
-            justifyContent="flex-end"
-            display="flex"
-            gap="1rem"
-            flexDirection="column"
-            width={isMobile ? "100%" : "auto"}
-          >
-            <Text
-              fontSize={isMobile ? "20px" : "38px"}
-              fontFamily="inter"
-              textAlign="center"
+    <Center>
+      <Box maxW="1440px" w="100%">
+        <Flex p="0.2rem" mt={isMobile ? "2rem" : "5rem"} id='proposal'>
+          <Box w="100%" position="relative">
+            <Flex
+              direction="column"
+              p="0 1rem"
+              w={isDesktop ? "100%" : "100%"}
               align="center"
+              justify="center"
+              mt={isMobile ? "4rem" : "0.3rem"}
+              flexWrap={getFlexWrap()}
+              position="relative"
+              gap="4rem"
+              textAlign="center"
               justifyContent="center"
-              fontWeight="700"
-              maxW={isMobile ? "" : "36rem"}
-              w="100%"
-              color="#f00"
             >
-              Nosso diferencial <Text color="#000000" as="span" m={0}>é ter você!</Text>
-            </Text>
+              <Box
+                flex="1"
+                textAlign="start"
+                alignItems="flex-start"
+                justifyContent="flex-end"
+                display="flex"
+                gap="1rem"
+                flexDirection="column"
+                width={isMobile ? "100%" : "auto"}
+              >
+                <Text
+                  fontSize={isMobile ? "20px" : "38px"}
+                  fontFamily="inter"
+                  textAlign="center"
+                  align="center"
+                  justifyContent="center"
+                  fontWeight="700"
+                  maxW={isMobile ? "" : "36rem"}
+                  w="100%"
+                  color="#f00"
+                >
+                  Nosso diferencial <Text color="#000000" as="span" m={0}>é ter você!</Text>
+                </Text>
+              </Box>
+
+              <Grid
+                templateColumns={getColumnTemplate()}
+                gap={4}
+                mt={6}
+                justifyContent="center"
+              >
+                <Box p={"0.2rem"}>
+                  <Center>
+                    <Image
+                      src={Intelligence}
+                      alt="Inteligência Artificial"
+                      w={isMobile ? "5rem" : "35%"}
+                      h="auto"
+                    />
+                  </Center>
+                  <Flex direction="column" alignItems="center" gap={"1rem"} mt={"1rem"}>
+                    <Text color="#000" fontFamily={"inter"} fontWeight={"700"} fontSize={"18px"}>Inteligência Artificial</Text>
+                    <Text color="#000" as="span">
+                      Com uma tecnologia exclusiva para melhorar nossa logística, fidelizamos os clientes com um atendimento mais eficiente.
+                    </Text>
+                  </Flex>
+                </Box>
+
+                <Box p={"0.2rem"}>
+                  <Center>
+                    <Image
+                      src={MoreClient}
+                      alt="Mais Clientes"
+                      w={isMobile ? "5rem" : "35%"}
+                      h="auto"
+                    />
+                  </Center>
+                  <Flex direction="column" alignItems="center" gap={"1rem"} mt={"1rem"}>
+                    <Text color="#000" fontFamily={"inter"} fontWeight={"700"} fontSize={"18px"}>Mais Clientes</Text>
+                    <Text color="#000" as="span">
+                      Aumentamos sua base de clientes para que você possa vender mais.
+                    </Text>
+                  </Flex>
+                </Box>
+
+                <Box p={"0.2rem"}>
+                  <Center>
+                    <Image
+                      src={Varieties}
+                      alt="Variedade"
+                      w={isMobile ? "5rem" : "35%"}
+                      h="auto"
+                    />
+                  </Center>
+                  <Flex direction="column" alignItems="center" gap={"1rem"} mt={"1rem"}>
+                    <Text color="#000" fontFamily={"inter"} fontWeight={"700"} fontSize={"18px"}>Variedade</Text>
+                    <Text color="#000" as="span">
+                      Não limitamos marcas, você tem a liberdade de escolher com base na sua clientela.
+                    </Text>
+                  </Flex>
+                </Box>
+
+                <Box p={"0.2rem"}>
+                  <Center>
+                    <Image
+                      src={Accessibility}
+                      alt="Acessibilidade"
+                      w={isMobile ? "5rem" : "35%"}
+                      h="auto"
+                    />
+                  </Center>
+                  <Flex direction="column" alignItems="center" gap={"1rem"} mt={"1rem"}>
+                    <Text color="#000" fontFamily={"inter"} fontWeight={"700"} fontSize={"18px"}>Acessibilidade</Text>
+                    <Text color="#000" as="span">
+                      Acesse o Mumu Delivery de qualquer lugar a qualquer hora com sistema fluido e totalmente responsivo.
+                    </Text>
+                  </Flex>
+                </Box>
+
+                <Box p={"0.2rem"}>
+                  <Center>
+                    <Image
+                      src={OfflineSupport}
+                      alt="Suporte Online"
+                      w={isMobile ? "5rem" : "35%"}
+                      h="auto"
+                    />
+                  </Center>
+                  <Flex direction="column" alignItems="center" gap={"1rem"} mt={"1rem"}>
+                    <Text color="#000" fontFamily={"inter"} fontWeight={"700"} fontSize={"18px"}>Suporte Online</Text>
+                    <Text color="#000" as="span">
+                      Conte conosco para ajudar em suas vendas e alavancar seus negócios.
+                    </Text>
+                  </Flex>
+                </Box>
+              </Grid>
+            </Flex>
           </Box>
-
-          <Grid
-            templateColumns={getColumnTemplate()}
-            gap={4}
-            mt={6}
-            justifyContent="center"
-          >
-            <Box p={"0.2rem"}>
-              <Center>
-                <Image
-                  src={Intelligence}
-                  alt="Inteligência Artificial"
-                  w={isMobile ? "5rem" : "35%"}
-                  h="auto"
-                />
-              </Center>
-              <Flex direction="column" alignItems="center" gap={"1rem"} mt={"1rem"}>
-                <Text color="#000" fontFamily={"inter"} fontWeight={"700"} fontSize={"18px"}>Inteligência Artificial</Text>
-                <Text color="#000" as="span">
-                  Com uma tecnologia exclusiva para melhorar nossa logística, fidelizamos os clientes com um atendimento mais eficiente.
-                </Text>
-              </Flex>
-            </Box>
-
-            <Box p={"0.2rem"}>
-              <Center>
-                <Image
-                  src={MoreClient}
-                  alt="Mais Clientes"
-                  w={isMobile ? "5rem" : "35%"}
-                  h="auto"
-                />
-              </Center>
-              <Flex direction="column" alignItems="center" gap={"1rem"} mt={"1rem"}>
-                <Text color="#000" fontFamily={"inter"} fontWeight={"700"} fontSize={"18px"}>Mais Clientes</Text>
-                <Text color="#000" as="span">
-                  Aumentamos sua base de clientes para que você possa vender mais.
-                </Text>
-              </Flex>
-            </Box>
-
-            <Box p={"0.2rem"}>
-              <Center>
-                <Image
-                  src={Varieties}
-                  alt="Variedade"
-                  w={isMobile ? "5rem" : "35%"}
-                  h="auto"
-                />
-              </Center>
-              <Flex direction="column" alignItems="center" gap={"1rem"} mt={"1rem"}>
-                <Text color="#000" fontFamily={"inter"} fontWeight={"700"} fontSize={"18px"}>Variedade</Text>
-                <Text color="#000" as="span">
-                  Não limitamos marcas, você tem a liberdade de escolher com base na sua clientela.
-                </Text>
-              </Flex>
-            </Box>
-
-            <Box p={"0.2rem"}>
-              <Center>
-                <Image
-                  src={Accessibility}
-                  alt="Acessibilidade"
-                  w={isMobile ? "5rem" : "35%"}
-                  h="auto"
-                />
-              </Center>
-              <Flex direction="column" alignItems="center" gap={"1rem"} mt={"1rem"}>
-                <Text color="#000" fontFamily={"inter"} fontWeight={"700"} fontSize={"18px"}>Acessibilidade</Text>
-                <Text color="#000" as="span">
-                  Acesse o Mumu Delivery de qualquer lugar a qualquer hora com sistema fluido e totalmente responsivo.
-                </Text>
-              </Flex>
-            </Box>
-
-            <Box p={"0.2rem"}>
-              <Center>
-                <Image
-                  src={OfflineSupport}
-                  alt="Suporte Online"
-                  w={isMobile ? "5rem" : "35%"}
-                  h="auto"
-                />
-              </Center>
-              <Flex direction="column" alignItems="center" gap={"1rem"} mt={"1rem"}>
-                <Text color="#000" fontFamily={"inter"} fontWeight={"700"} fontSize={"18px"}>Suporte Online</Text>
-                <Text color="#000" as="span">
-                  Conte conosco para ajudar em suas vendas e alavancar seus negócios.
-                </Text>
-              </Flex>
-            </Box>
-            
-          </Grid>
         </Flex>
       </Box>
-    </Flex>
+    </Center>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,22 @@
 import { Flex, Text, Box, Image, Link, useMediaQuery } from "@chakra-ui/react";
-import mumulogo from '../../images/mumu3 1.svg'
-import facebookImg from '../../images/facebook_svgrepo.com.svg'
-import instagramImg from '../../images/instagram-1_svgrepo.com.svg'
-import appStore from '../../images/AppStore.svg'
-import GooglePlayStore from '../../images/GooglePlayStore.svg'
+import mumulogo from "../../images/mumu3 1.svg";
+import facebookImg from "../../images/facebook_svgrepo.com.svg";
+import instagramImg from "../../images/instagram-1_svgrepo.com.svg";
+import appStore from "../../images/AppStore.svg";
+import GooglePlayStore from "../../images/GooglePlayStore.svg";
 
 export default function Footer() {
-
   const [isMobile] = useMediaQuery("(max-width: 768px)");
   const [isDesktop] = useMediaQuery("(max-width: 1024px)");
 
   return (
-    <Box bg="#E5E5E587" id="footer">
+    <Flex
+      justify="center"
+      align="center"
+      direction="column"
+      bg="#E5E5E587"
+      id="footer"
+    >
       <Flex
         direction={isMobile ? "column" : "row"}
         justify="center"
@@ -22,6 +27,8 @@ export default function Footer() {
         flexWrap={isMobile ? "none" : "wrap"}
         gap={isMobile ? "0" : "5rem"}
         alignItems={isMobile ? "center" : "baseline"}
+        maxW="1440px"
+        w="100%"
       >
         <Flex
           direction="column"
@@ -30,9 +37,9 @@ export default function Footer() {
           w={isMobile ? "100%" : "21%"}
         >
           <Link>
-            <Image src={mumulogo} alt="logo" /> 
+            <Image src={mumulogo} alt="logo" />
           </Link>
-          <Box fontSize={5}  alignSelf={isMobile ? "none" : "flex-end"}>
+          <Box fontSize={5} alignSelf={isMobile ? "none" : "flex-end"}>
             <Text fontSize={13} color="#000">
               contato@mumudelivery.com
             </Text>
@@ -70,20 +77,19 @@ export default function Footer() {
           align={isMobile ? "center" : "flex-start"}
           mb={{ base: "0rem", md: "0" }}
         >
-          <Text fontWeight={700} mb="0.5rem" color={"#f00"} >
+          <Text fontWeight={700} mb="0.5rem" color={"#f00"}>
             Termos e Política
           </Text>
           <Link color="#000">Termos de Uso</Link>
           <Link color="#000">Política de Privacidade</Link>
         </Flex>
 
-
         <Flex
           direction="column"
           align={isMobile ? "center" : "flex-start"}
           mb={{ base: "2rem", md: "0" }}
         >
-          <Text fontWeight={700} mb="0.5rem" color={"#f00"} >
+          <Text fontWeight={700} mb="0.5rem" color={"#f00"}>
             Termos e Política
           </Text>
           <Link>
@@ -93,37 +99,53 @@ export default function Footer() {
             <Image src={GooglePlayStore} alt="Get it on Google Play" />
           </Link>
         </Flex>
-     
       </Flex>
-      <Flex direction="column" align="center" justify="center" p={isMobile ? "1rem" : "2rem"} borderWidth="1%">
-      <hr
-        style={{ borderTop: '1px solid rgba(0, 0, 0, 0.292)', width: isDesktop ? '100%' : '100%' }}
-      />
-      <Flex direction={isMobile ? 'column' : 'row'} justifyContent="center" p={isMobile ? "0rem" : "2rem"} >
-        <Text
-          fontSize={12}
-          color="#000"
-          textAlign={isMobile ? "center" : "inherit"}
-          mt={{ base: '2rem', md: '2rem' }}
-          mb={{ base: '1rem', md: '0' }}
-          w={isMobile ? "100%" : "44%"}
+      <Flex
+        direction="column"
+        align="center"
+        justify="center"
+        p={isMobile ? "1rem" : "2rem"}
+        borderWidth="1%"
+      >
+        <hr
+          style={{
+            borderTop: "1px solid rgba(0, 0, 0, 0.292)",
+            width: isDesktop ? "100%" : "100%",
+          }}
+        />
+        <Flex
+          direction={isMobile ? "column" : "row"}
+          justifyContent="center"
+          p={isMobile ? "0rem" : "2rem"}
+          maxW="1440px"
+          w="100%"
         >
-          Nossos pés estão no chão, mas nossas ambições descoladas estão acima das nuvens.
-          Aqui está como nos movemos para satisfazer nossos clientes.
-        </Text>
+          <Text
+            fontSize={12}
+            color="#000"
+            textAlign={isMobile ? "center" : "inherit"}
+            mt={{ base: "2rem", md: "2rem" }}
+            mb={{ base: "1rem", md: "0" }}
+            w={isMobile ? "100%" : "44%"}
+          >
+            Nossos pés estão no chão, mas nossas ambições descoladas estão acima
+            das nuvens. Aqui está como nos movemos para satisfazer nossos
+            clientes.
+          </Text>
 
-        <Text
-          fontSize={12}
-          color="#000"
-          textAlign={isMobile ? "center" : "end"}
-          mt={{ base: '2rem', md: '2rem' }}
-          mb={{ base: '1rem', md: '0' }}
-          w={isMobile ? "100%" : "33%"}
-        >
-          Mumu Delivery © 2023 - Todos os direitos reservados - CNPJ: 00.000.000/0000-00
-        </Text>
+          <Text
+            fontSize={12}
+            color="#000"
+            textAlign={isMobile ? "center" : "end"}
+            mt={{ base: "2rem", md: "2rem" }}
+            mb={{ base: "1rem", md: "0" }}
+            w={isMobile ? "100%" : "33%"}
+          >
+            Mumu Delivery © 2023 - Todos os direitos reservados - CNPJ:
+            00.000.000/0000-00
+          </Text>
+        </Flex>
       </Flex>
     </Flex>
-    </Box>
   );
 }
